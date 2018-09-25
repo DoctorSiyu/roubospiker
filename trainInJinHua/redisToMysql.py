b530c92f5c55318391dba5db1d8b4be322d26c61
daijunjian <daijunjian11@gmail.com>
2018-09-25 14:48:23 +0800

    完成并集、增加部分、减少部分等计算
    Change-Id: I7890dce3b3a812610f4f8795c835760953a7b966

--- trainInJinHua/redisToMysql.py
+++ trainInJinHua/redisToMysql.py
@@ -34,40 +34,132 @@ def connectMysql():
     return engine
 
 
-def redisToMysql(re, en):
+def redisToMysql(re, en, redis_key, mysql_table, is_append):
     """
     :param re: redis connect
     :param en: mysql engine connect
+    :param redis_key: mysql engine connect
+    :param mysql_table: mysql engine connect
     :return:
     """
     today = time.strftime("%Y_%m_%d")
-    tableName = tool.getFileKey() + '_' + today
     res = []
     index = 0
-    for item in re.sscan_iter(tool.getFileKey() + '_' + today):
+    for item in re.sscan_iter(redis_key):
         tmp = eval(item.encode('utf-8').decode('utf-8'))
         tmp['time'] = today
         res.append(tmp)
         index += 1
         if index >= 100:
             df = pd.DataFrame(res)
-            df.to_sql('respage01', con=en, if_exists='append', index=False,)
+            if is_append:
+                df.to_sql(mysql_table, con=en, if_exists='append', index=False)
+            else:
+                df.to_sql(mysql_table, con=en, if_exists='replace', index=False)
             index = 0
             res = []
     if index != 0:
         df = pd.DataFrame(res)
-        df.to_sql(name='respage01', con=en, if_exists='append', index=False)
+        if is_append:
+            df.to_sql(name=mysql_table, con=en, if_exists='append', index=False)
+        else:
+            df.to_sql(name=mysql_table, con=en, if_exists='replace', index=False)
 
-    # 添加主键
-    # print("xxxxxxxx")
-    # with en.connect() as con:
-    #     con.execute("alter table respage01 add COLUMN id INT NOT NULL AUTO_INCREMENT  primary key first")
+    try:
+        with en.connect() as con:
+            con.execute("alter table " + mysql_table + " add COLUMN id INT NOT NULL AUTO_INCREMENT  primary key first")
+    except Exception as e:
+        print('add id error')
+
+
+def getKeyList(re):
+    """
+    获取所有的key的列表
+    :return:
+    """
+    return re.keys(tool.getFileKey() + "_*")
+
+
+def getKeyListExceptToday(re):
+    """
+    获取除了今天以外的所有key的列表
+    :return:
+    """
+    today = time.strftime("%Y_%m_%d")
+    today = '2018_09_09'
+    keyList = re.keys(tool.getFileKey() + '_*')
+    try:
+        keyList.remove(tool.getFileKey() + '_' + today)
+    except Exception as e:
+        pass
+    return keyList
+
+
+def getUnionData(re):
+    """
+    获得所有数据的并集并保存到trainunion中
+    :param re:
+    :return:
+    """
+    keyList = getKeyList(re)
+    re.sunionstore(tool.getFileKey() + 'union', keyList)
+    try:
+        re.delete(tool.getFileKey() + 'unionmp')
+    except Exception as e:
+        pass
+    finally:
+        re.sunionstore(tool.getFileKey() + 'uniontmp', getKeyListExceptToday(re))
+
+
+def getNewDiffData(re):
+    """
+    获取新增的数据
+    :param re:
+    :return:
+    """
+    today = time.strftime("%Y_%m_%d")
+    today = '2018_09_09'
+    if 'train_' + today in getKeyList(re):
+        re.delete(tool.getFileKey() + 'new')
+        re.sdiffstore(tool.getFileKey() + 'new', tool.getFileKey() + '_' + today, tool.getFileKey() + 'uniontmp')
+    else:
+        pass
+
+
+def getGoneDiffData(re):
+    """
+    获取消失的数据
+    :param re:
+    :return:
+    """
+    today = time.strftime("%Y_%m_%d")
+    today = '2018_09_09'
+    if tool.getFileKey() + '_' + today in getKeyList(re):
+        re.delete(tool.getFileKey() + 'gone')
+        re.sdiffstore(tool.getFileKey() + 'gone', tool.getFileKey() + 'union', tool.getFileKey() + '_' + today)
+    else:
+        pass
 
 
 def main():
+    today = time.strftime("%Y_%m_%d")
+    todayKey = tool.getFileKey() + '_' + today
+    newKey = tool.getFileKey() + 'new'
+    goneKey = tool.getFileKey() + 'gone'
+    unionKey = tool.getFileKey() + 'union'
     re = connectRedis()
     en = connectMysql()
-    redisToMysql(re, en)
+    getUnionData(re)
+    getGoneDiffData(re)
+    getNewDiffData(re)
+    # 将今日数据写入mysql
+    redisToMysql(re, en, todayKey, 'respage01', True)
+    # 将今日新增数据写入mysql
+    redisToMysql(re, en, newKey, 'respage01New', False)
+    # 将今日消失数据写入mysql
+    redisToMysql(re, en, goneKey, 'respage01Gone', False)
+    # 将并集数据写入mysql
+    redisToMysql(re, en, unionKey, 'respage01Union', False)
 
 
 if __name__ == '__main__':
